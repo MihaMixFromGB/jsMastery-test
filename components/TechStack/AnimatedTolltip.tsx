52fae847d207a5076452a7ed543a4382e673190c
an.dehterev <an.dehterev>
2024-07-19 17:35:37 +0700

    fix styles

--- components/TechStack/AnimatedTolltip.tsx
+++ components/TechStack/AnimatedTolltip.tsx
@@ -67,7 +67,7 @@ const AnimatedTooltip: React.FC<AnimatedTooltipProps> = ({ items }) => {
                   rotate: rotate,
                   whiteSpace: "nowrap",
                 }}
-                className="absolute -top-14 left-1/2 -translate-x-1/2 flex text-xs flex-col items-center justify-center rounded-md bg-black z-50 shadow-xl px-4 py-2"
+                className="absolute -top-14 left-1/2 flex text-xs flex-col items-center justify-center rounded-md bg-black z-50 shadow-xl px-4 py-2"
               >
                 <div className="absolute z-30 w-[20%] -bottom-px bg-gradient-to-r from-transparent via-emerald-500 to-transparent h-px" />
                 <div className="absolute w-[40%] z-30 -bottom-px bg-gradient-to-r from-transparent via-sky-500 to-transparent h-px" />
@@ -83,7 +83,7 @@ const AnimatedTooltip: React.FC<AnimatedTooltipProps> = ({ items }) => {
             width={64}
             src={item.image}
             alt={item.name}
-            className="object-cover !m-0 !p-0 object-top rounded-full h-14 w-14 border-2 group-hover:scale-105 group-hover:z-30 border-white  relative transition duration-500 bg-white"
+            className="object-contain !m-0 !p-0 object-top rounded-full h-14 w-14 border-2 group-hover:scale-105 group-hover:z-30 border-gray-200 relative transition duration-500 bg-white"
           />
         </div>
       ))}
